0db6856a0a8414309a41f2b3b260e3474e69e078
elitmusss <105987750+elitmusss@users.noreply.github.com>
2022-05-21 23:12:26 +0530

    Update index.js

--- src/Assets/Constants/index.js
+++ src/Assets/Constants/index.js
@@ -1,2 +1,2 @@
-// export const BACKEND_URL = "https://hack387.herokuapp.com/";
-export const BACKEND_URL = "http://localhost:3870/";
+export const BACKEND_URL = "https://hack387.herokuapp.com/";
+//export const BACKEND_URL = "http://localhost:3870/";
