cdb143dc47bc50a4fb96c282253da0fe6952a9c1
mkp0 <shadymasum@gmail.com>
2022-05-21 23:30:08 +0530

    yo2

--- src/Assets/Routes/Routes.js
+++ src/Assets/Routes/Routes.js
@@ -1,8 +1,6 @@
 import HomePage from "../../Pages/HomePage/HomePage";
 import SignUp from "../../Pages/SignUp/SignUp";
 import LogIn from "../../Pages/LogIn/Login";
-import Game1 from "../../Pages/Game1/Game";
-import Game3 from "../../Pages/Game3/Game3";
 import Game2 from "../../Pages/Game2/Game2";
 import Profile from "../../Pages/Profile/Profile";
 import Logout from "../../Pages/Logout/Logout";
@@ -11,18 +9,6 @@ export const Routes = [
     id: 1,
     component: HomePage,
     path: "/",
-    exact: true,
-  },
-  {
-    id: 2,
-    component: Game1,
-    path: "/game1",
-    exact: true,
-  },
-  {
-    id: 3,
-    component: Game3,
-    path: "/game3",
     exact: true,
   },
   {
